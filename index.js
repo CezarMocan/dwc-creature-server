d4fc421438c21f92e517880c19a7394da83ea684
Cezar Mocan <mocancezar@gmail.com>
2019-03-02 18:38:28 -0500

    cors

--- index.js
+++ index.js
@@ -4,6 +4,16 @@ var app = require('express')()
 var http = require('http').Server(app)
 var io = require('socket.io')(http)
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.get('/', function(req, res, next) {
+    res.send('<h1>Hello world</h1>');
+});
+
 http.listen(3001, function(){
   console.log('listening on *:3001')
 });
