e0611c9e05487a6498e5d51d2d4c728d556bdcba
Jonathan Dahan <hi@jonathan.is>
2019-03-13 19:10:48 -0400

    initial dwc-creature / dat integration

--- index.js
+++ index.js
@@ -1,64 +1,81 @@
 import { manager } from './manager'
 import bodyParser from 'body-parser'
-
-var app = require('express')()
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
-
-var http = require('http').Server(app)
-var io = require('socket.io')(http)
-var port = process.env.PORT || 3001;
-
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-})
-
-http.listen(port, function(){
-  console.log('listening on *:3001')
-})
+import { Creature } from 'dwc-creature'
 
 
-// A client connected to the garden
-io.on('connection', (socket) => {
-  manager.addClient(socket)
-})
+let creature = new Creature({ initialData: { face: ':)' } })
+creature.on('updated', console.dir)
 
-// Creature entering the garden
-// curl -d 'creature=2' http://localhost:3001/hello
-app.post('/hello', (req, res, next) => {
-  const creatureId = req.body.creature
+creature.on('created', innerCreature => {
+  var app = require('express')()
+  app.use(bodyParser.json())
+  app.use(bodyParser.urlencoded({ extended: true }))
 
-  if (!creatureId) {
-    const msg = 'Warning: The request does not have a "creature" parameter'
-    console.warn(msg)
+  var http = require('http').Server(app)
+  var io = require('socket.io')(http)
+  var port = process.env.PORT || 3001;
+
+  app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  })
+
+  http.listen(port, function () {
+    console.log('listening on *:3001')
+  })
+
+  // A client connected to the garden
+  io.on('connection', (socket) => {
+    manager.addClient(socket)
+  })
+
+  app.get('/creature', (req, res, next) => {
+    res.send(creature.data())
+  })
+
+  app.post('/creature', (req, res, next) => {
+    creature.update(req.body)
+    res.send(creature.data())
+  })
+
+  // Creature entering the garden
+  // curl -d 'creature=2' http://localhost:3001/hello
+  app.post('/hello', (req, res, next) => {
+    const creatureId = req.body.creature
+
+    if (!creatureId) {
+      const msg = 'Warning: The request does not have a "creature" parameter'
+      console.warn(msg)
+      res.send(msg)
+      return
+    }
+
+    manager.helloCreature(creatureId)
+
+    const msg = 'Creature ' + creatureId + ' has successfully entered the garden'
+    console.log(msg)
     res.send(msg)
-    return
-  }
+  })
 
-  manager.helloCreature(creatureId)
+  // Creature leaving the garden
+  // curl -d 'creature=2' http://localhost:3001/goodbye
+  app.post('/goodbye', (req, res, next) => {
+    const creatureId = req.body.creature
 
-  const msg = 'Creature ' + creatureId + ' has successfully entered the garden'
-  console.log(msg)
-  res.send(msg)
-})
+    if (!creatureId) {
+      const msg = 'Warning: The request does not have a "creature" parameter'
+      console.warn(msg)
+      res.send(msg)
+      return
+    }
 
-// Creature leaving the garden
-// curl -d 'creature=2' http://localhost:3001/goodbye
-app.post('/goodbye', (req, res, next) => {
-  const creatureId = req.body.creature
+    manager.goodbyeCreature(creatureId)
 
-  if (!creatureId) {
-    const msg = 'Warning: The request does not have a "creature" parameter'
-    console.warn(msg)
+    const msg = 'Creature ' + creatureId + ' has successfully left the garden'
+    console.log(msg)
     res.send(msg)
-    return    
-  }
+  })
 
-  manager.goodbyeCreature(creatureId)
-  
-  const msg = 'Creature ' + creatureId + ' has successfully left the garden'
-  console.log(msg)
-  res.send(msg)
 })
+creature.sync()
